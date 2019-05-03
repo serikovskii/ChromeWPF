4c0a99d9e5c172923c075ddbbfccf052452c2713
Azat <46015824+serikovskii@users.noreply.github.com>
2019-05-03 11:49:26 +0600

    доделать переключение табов и +

---
+++ ChromeWpf/ChromeWpf/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ChromeWpf
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AddTab(object sender, MouseButtonEventArgs e)
+        {
+            var web = new WebBrowser
+            {
+                Source = new Uri("https://google.kz")
+            };
+
+            var textBlock = new TextBlock
+            {
+                Text = "Новая вкладка"
+            };
+
+            var newTab = new TabItem
+            {
+                Header = textBlock, 
+                Content = web
+            };
+
+            var indexAddNewTab = tabs.Items.Count - 1;
+            var lastAddNewTab = tabs.Items[indexAddNewTab];
+
+            tabs.Items.RemoveAt(indexAddNewTab);
+            tabs.Items.Add(newTab);
+            tabs.Items.Add(lastAddNewTab);
+
+
+        }
+    }
+}
